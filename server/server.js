8b640d52336c889b784bc958233679b57239cb9a
AATHEESWARAN-S <aathees.rs@gmail.com>
2026-01-09 08:08:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,12 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
 
-app.use("/api/contacts", require("./routes/contactRoutes"));
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.get("/", (req, res) => {
+  res.send("Contact Manager API Running");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
